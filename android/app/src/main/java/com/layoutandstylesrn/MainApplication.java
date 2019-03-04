1410a86327764fae14778e6e7d5b291584d7b3af
anisa07 <askany07@gmail.com>
2019-03-04 17:46:26 +0300

    react-native-keychain is added

--- android/app/src/main/java/com/layoutandstylesrn/MainApplication.java
+++ android/app/src/main/java/com/layoutandstylesrn/MainApplication.java
@@ -3,6 +3,7 @@ package com.layoutandstylesrn;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.keychain.KeychainPackage;
 import com.airbnb.android.react.lottie.LottiePackage;
 import com.airbnb.android.react.lottie.LottiePackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new KeychainPackage(),
           new LottiePackage(),
           new RNGestureHandlerPackage(),
           new MapsPackage(),
